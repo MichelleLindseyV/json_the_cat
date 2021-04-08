d0612beea0e667f02f6fa98266f1e62044efa50c
Michelle Verge <michellelverge@gmail.com>
2021-04-08 23:54:14 +0000

    code refactoring and testing with mocha/chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 
-
 const request = require('request');
 
-let userInput = process.argv[2];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error, response, body) => {
-  console.log('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
-
-
-  const data = JSON.parse(body);
-  console.log(data[0]["description"]);
-  // console.log(typeof data);
-  
-});
-
-// if (userInput === "undefined") {
-  //   console.log("Breed not found")
-  //   return;
-  // } else {
  
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback("Breed not found", null);
+      }
+      let desc = (data[0]["description"]);
+      return callback(null, desc);
+    }
+    //console.log('statusCode:', response && response.statusCode);
+  });
+};
+
+module.exports = { fetchBreedDescription };
